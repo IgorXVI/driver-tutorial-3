25e4c5248e100ef57ca40a58d77b70cbe78274bc
Igor Almeida <inazumaseleven04@gmail.com>
2023-10-24 17:35:33 -0300

    init and exit

--- hello-1.c
+++ hello-1.c
@@ -1,14 +1,20 @@
+#include <linux/init.h>
 #include <linux/module.h>
 #include <linux/printk.h>
 
 MODULE_LICENSE("GPL");
 
-int init_module(void) {
+static int __init hello_2_init(void)
+{
     pr_info("Hello world.\n");
 
     return 0;
 }
 
-void cleanup_module(void) {
+static void __exit hello_2_exit(void)
+{
     pr_info("Tchau world.\n");
 }
+
+module_init(hello_2_init);
+module_exit(hello_2_exit);
